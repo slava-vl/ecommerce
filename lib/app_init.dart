a4ab5bc60014d6405d039929d03041019f7e1233
melanch0lic <kudza76@mail.ru>
2022-10-06 18:14:10 +0300

    kill init

--- lib/app_init.dart
+++ lib/app_init.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:dio/dio.dart';
 
-import 'init_provider.dart';
+import 'data/api_client.dart';
+import 'domain/services/cart_data_service.dart';
+import 'domain/services/home_data_service.dart';
+import 'domain/services/product_detail_data_service.dart';
+import 'navigation/main_navigation.dart';
 import 'navigation/main_navigation_route_names.dart';
 import 'pages/cart_page/cart_page_model.dart';
 import 'pages/home_page/home_page_model.dart';
@@ -37,13 +42,13 @@ class AppInitialization extends StatelessWidget {
                   theme: ThemeData(
                       scaffoldBackgroundColor: const Color.fromRGBO(248, 248, 248, 1),
                       primaryColor: const Color.fromRGBO(255, 110, 78, 1),
-                      accentColor: const Color.fromRGBO(1, 0, 53, 1),
                       textTheme: const TextTheme(
                         bodySmall: TextStyle(fontWeight: FontWeight.w400),
                         bodyMedium: TextStyle(fontWeight: FontWeight.w500),
                         bodyLarge: TextStyle(fontWeight: FontWeight.w700),
                         headlineLarge: TextStyle(fontWeight: FontWeight.w800),
-                      )),
+                      ),
+                      colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color.fromRGBO(1, 0, 53, 1))),
                   routes: context.read<InitializeProvider>().navigation.routes,
                   initialRoute: MainNavigationRouteNames.splashRoute,
                 ),
@@ -53,5 +58,30 @@ class AppInitialization extends StatelessWidget {
         );
       }),
     );
+  }
+}
+
+class InitializeProvider with ChangeNotifier {
+  late final MainNavigation _navigation;
+  MainNavigation get navigation => _navigation;
+
+  late final ApiClient _apiClient;
+  ApiClient get apiClient => _apiClient;
+
+  late final HomeDataService _homeDataService;
+  HomeDataService get homeDataService => _homeDataService;
+
+  late final ProductDetailDataSrvice _productDetailDataSrvice;
+  ProductDetailDataSrvice get productDetailDataSrvice => _productDetailDataSrvice;
+
+  late final CartDataSrvice _cartDataSrvice;
+  CartDataSrvice get cartDataSrvice => _cartDataSrvice;
+
+  Future<void> initializeApp() async {
+    _navigation = MainNavigation();
+    _apiClient = ApiClient(Dio());
+    _homeDataService = HomeDataService(apiClient: _apiClient);
+    _productDetailDataSrvice = ProductDetailDataSrvice(apiClient: _apiClient);
+    _cartDataSrvice = CartDataSrvice(apiClient: _apiClient);
   }
 }
